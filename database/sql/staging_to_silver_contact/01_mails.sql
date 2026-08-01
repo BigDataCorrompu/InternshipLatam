2c60b3150ee2e2a883d637089a682088a8042794
BigDataCorrompu <roland.oucherif@gmail.com>
2026-08-01 06:41:42 -0400

    fix find mails

--- database/sql/staging_to_silver_contact/01_mails.sql
+++ database/sql/staging_to_silver_contact/01_mails.sql
@@ -1,18 +1,19 @@
-INSERT INTO silver.company_contacts (id_company, id_location, email, score, reason, source, found_at)
-SELECT DISTINCT ON (id_company, email)
+INSERT INTO analytics.company_contact (id_company, id_location, email, confidence, explanation, source, collected_at)
+SELECT DISTINCT ON (email)
     se.id_company,
     se.id_location,
     (contact->>'email')::TEXT,
-    (contact->>'score')::NUMERIC(3,2),
+    (contact->>'score')::FLOAT,
     contact->>'reason',
     contact->>'source',
     se.collected_at
 FROM staging.company_emails se,
-        jsonb_array_elements(se.raw_result) AS contact
-ORDER BY id_company, email, (contact->>'score')::NUMERIC(3,2) DESC, se.collected_at DESC
-ON CONFLICT (id_company, email) DO UPDATE
-    SET score = EXCLUDED.score,
-        reason = EXCLUDED.reason,
+     jsonb_array_elements(se.raw_result) AS contact
+WHERE contact->>'email' IS NOT NULL
+ORDER BY email, (contact->>'score')::FLOAT DESC, se.collected_at DESC
+ON CONFLICT (email) DO UPDATE
+    SET confidence = EXCLUDED.confidence,
+        explanation = EXCLUDED.explanation,
         source = EXCLUDED.source,
-        found_at = EXCLUDED.found_at
-    WHERE EXCLUDED.score > silver.company_contacts.score
+        collected_at = EXCLUDED.collected_at
+    WHERE EXCLUDED.confidence > analytics.company_contact.confidence
